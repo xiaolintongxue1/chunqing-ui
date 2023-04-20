79671cf5882ac6e028605ba6cb625239514c0847
lin chunqing <chunqinglin@hokdo.com>
2023-04-20 16:14:12 +0800

    feat: packages/comonents 库模式打包

--- demo/app.vue
+++ demo/app.vue
@@ -1,6 +1,7 @@
 <template>
     <div>启动测试</div>
-    <ea-button />
+    <ea-button type="primary">我是有样式的按钮</ea-button>
+    <!-- <el-button></el-button> -->
   </template>
   <script lang="ts" setup>
   </script>
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // @ts-ignore
 import DefineOptions from "unplugin-vue-define-options/vite";
+// @ts-ignore
+import ElementPlus from 'unplugin-element-plus/vite'
+
 export default defineConfig({
-  plugins: [vue(), DefineOptions()],
+  plugins: [vue(), DefineOptions(), ElementPlus()],
 });
