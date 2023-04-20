d97856eb9f0dc2c4053deb7ecfa28d4eb9622b52
lin chunqing <chunqinglin@hokdo.com>
2023-04-20 16:12:20 +0800

    feat: components vite 库模式打包

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+//@ts-ignore
+import DefineOptions from "unplugin-vue-define-options/vite";
 
 export default defineConfig({
   build: {
@@ -11,19 +15,43 @@ export default defineConfig({
       //忽略打包vue文件
       external: ["vue"],
       //input: ["index.ts"],
-      output: {
-        globals: {
-          vue: "Vue",
+      output: [
+        {
+          //打包格式
+          format: "es",
+          //打包后文件名
+          entryFileNames: "[name].mjs",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "../chunqing-ui/es",
         },
-        dir: "dist",
-      },
+        {
+          //打包格式
+          format: "cjs",
+          //打包后文件名
+          entryFileNames: "[name].js",
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: "named",
+          //配置打包根目录
+          dir: "../chunqing-ui/lib",
+        },
+      ],
     },
     lib: {
-      entry: "./index.ts",
-      name: "chunqing-ui",
-      fileName: "chunqing-ui",
-      formats: ["es", "umd", "cjs"],
+      entry: "./index.ts"
     },
   },
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        dts({
+            entryRoot: "./src",
+            outputDir: ["../chunqing-ui/es/src", "../chunqing-ui/lib/src"],
+            //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+            tsConfigFilePath: "../../tsconfig.json",
+        }),
+        DefineOptions()
+    ],
 });
